52f73163f6b2692b2042d5bb676ae756d295f010
Nazarsenychak <nazaesenychak1@gmail.com>
2020-02-19 13:33:23 +0200

    Updates

---
+++ precache-manifest.1721d456ffc6d2e8eca0e4e2a83aa9c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c873c11b04c321ef1e27861647c5881",
+    "url": "/charts-4service/index.html"
+  },
+  {
+    "revision": "59d5ebd362f756e1d114",
+    "url": "/charts-4service/static/css/main.43e69d1d.chunk.css"
+  },
+  {
+    "revision": "c3322222f343851f5b2c",
+    "url": "/charts-4service/static/js/2.67b0b4b2.chunk.js"
+  },
+  {
+    "revision": "a22a7ae5fe503af7d2f4bea67c7372bc",
+    "url": "/charts-4service/static/js/2.67b0b4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d5ebd362f756e1d114",
+    "url": "/charts-4service/static/js/main.c57a6815.chunk.js"
+  },
+  {
+    "revision": "f96e9c868b4380da395a",
+    "url": "/charts-4service/static/js/runtime-main.4f64ae97.js"
+  },
+  {
+    "revision": "b1d2448888e302eaf2b19debe916a04c",
+    "url": "/charts-4service/static/media/horse.b1d24488.png"
+  }
+]);
